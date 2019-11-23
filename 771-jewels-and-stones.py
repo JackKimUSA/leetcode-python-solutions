fd1b39907c802f6214b655cad9564d8a366ade55
Jack Kim <jackkimusa@gmail.com>
2019-11-22 22:57:50 -0800

    Create 771-jewels-and-stones.py
    Add 771-jewels-and-stones.py file

---
+++ 771-jewels-and-stones.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 22:45:55 2019
+
+@author: jgkim
+"""
+
+"""
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.
+Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, 
+so "a" is considered a different type of stone from "A".
+"""
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count=0
+        for j in J:
+            count += S.count(j)
+        return count
+
+J="aA"
+S="aAAbbbb"
+Sol=Solution()
+print(Sol.numJewelsInStones(J,S))
+
+"""
+Submissions:
+Runtime: 40 ms, faster than 39.99% of Python3 online submissions for Jewels and Stones.
+Memory Usage: 12.8 MB, less than 100.00% of Python3 online submissions for Jewels and Stones.
+"""
